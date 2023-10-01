cfbe3a2af3aa693c8609506224b449c9ab91787c
Leonid <lkwebdev@mail.ru>
2023-10-01 17:16:52 +0300

    RSE: исправление на использование хука useContext

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,26 +1,25 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import styles from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
-    return (<AuthContext.Consumer>
-        {(ctx) => {
-            return (<nav className={styles.nav}>
-                <ul>
-                    {ctx.isLoggedIn && (<li>
-                        <a href="/">Пользователи</a>
-                    </li>)}
-                    {ctx.isLoggedIn && (<li>
-                        <a href="/">Админ</a>
-                    </li>)}
-                    {ctx.isLoggedIn && (<li>
-                        <button onClick={props.onLogout}>Выйти</button>
-                    </li>)}
-                </ul>
-            </nav>);
-        }}
-    </AuthContext.Consumer>);
+    const ctx = useContext(AuthContext);
+
+
+    return (<nav className={styles.nav}>
+        <ul>
+            {ctx.isLoggedIn && (<li>
+                <a href="/">Пользователи</a>
+            </li>)}
+            {ctx.isLoggedIn && (<li>
+                <a href="/">Админ</a>
+            </li>)}
+            {ctx.isLoggedIn && (<li>
+                <button onClick={props.onLogout}>Выйти</button>
+            </li>)}
+        </ul>
+    </nav>);
 };
 
 export default Navigation;
