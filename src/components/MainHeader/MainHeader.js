48265414d58849cd482959802c5868a3a25ea60a
Leonid <lkwebdev@mail.ru>
2023-10-01 16:59:22 +0300

    RSE: завершение 104 урока

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -8,7 +8,6 @@ const MainHeader = (props) => {
     <header className={styles["main-header"]}>
       <h1>React Advanced</h1>
       <Navigation
-        isLoggedIn={props.isAuthenticated}
         onLogout={props.onLogout}
       />
     </header>
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,26 @@
 import React from "react";
 
 import styles from "./Navigation.module.css";
+import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
-  return (
-    <nav className={styles.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Пользователи</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Админ</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Выйти</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+    return (<AuthContext.Consumer>
+        {(ctx) => {
+            return (<nav className={styles.nav}>
+                <ul>
+                    {ctx.isLoggedIn && (<li>
+                        <a href="/">Пользователи</a>
+                    </li>)}
+                    {ctx.isLoggedIn && (<li>
+                        <a href="/">Админ</a>
+                    </li>)}
+                    {ctx.isLoggedIn && (<li>
+                        <button onClick={props.onLogout}>Выйти</button>
+                    </li>)}
+                </ul>
+            </nav>);
+        }}
+    </AuthContext.Consumer>);
 };
 
 export default Navigation;
